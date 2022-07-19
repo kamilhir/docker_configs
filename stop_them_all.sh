b7a52e379dec5c6d3c2c484baa7424c59710a56d
Kamil Hir <kamil.hir@gmail.com>
2022-07-19 21:36:02 +0200

    Few modification

--- stop_them_all.sh
+++ stop_them_all.sh
@@ -8,3 +8,6 @@ docker compose -f portainer/compose.yml down
 docker compose -f nginx/compose.yml down
 docker compose -f nginxpm/compose.yml down
 docker compose -f cors-container/compose.yml down
+
+# Remove unused networks
+docker network prune
