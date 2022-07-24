31d4234ce42a7fe774cc609aee368ffad2eb068a
Kamil Hir <kamil.hir@gmail.com>
2022-07-24 11:26:57 +0200

    Remobing unused containers

--- run_them_all.sh
+++ run_them_all.sh
@@ -5,10 +5,6 @@
 
 #Create containers. Comment with # if you dont need something:
 
-#docker compose -f nginx/compose.yml up -d
-#docker compose -f nginxpm/compose.yml up -d
-#docker compose -f cors-container.yml up -d
-docker compose -f homer/compose.yml up -d
 docker compose -f heimdall/compose.yml up -d
 docker compose -f jellyfin/compose.yml up -d
 docker compose -f portainer/compose.yml up -d
--- stop_them_all.sh
+++ stop_them_all.sh
@@ -1,13 +1,9 @@
 # Basic script to destroy all containers.
 
 # Remove everything:
-docker compose -f homer/compose.yml down
 docker compose -f heimdall/compose.yml down
 docker compose -f jellyfin/compose.yml down
 docker compose -f portainer/compose.yml down
-docker compose -f nginx/compose.yml down
-docker compose -f nginxpm/compose.yml down
-docker compose -f cors-container/compose.yml down
 
 # Remove unused networks
 docker network prune
